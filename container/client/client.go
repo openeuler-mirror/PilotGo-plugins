4a8df5f388e9f9ac37b37a83f7f5cecace7cb002
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-05-23 14:38:55 +0800

    add container plugin

---
+++ container/client/client.go
@@ -0,0 +1,30 @@
+package client
+
+import (
+	splugin "gitee.com/openeuler/PilotGo-plugins/sdk/plugin"
+	"openeuler.org/PilotGo/container-plugin/config"
+)
+
+const Version = "0.0.1"
+
+var globalClient *splugin.Client
+
+func init() {
+	globalClient = splugin.DefaultClient(&splugin.PluginInfo{
+		Name:        "container",
+		Version:     Version,
+		Description: "Container management plugin",
+		Author:      "wangjunqi",
+		Email:       "wangjunqi@kylinos.cn",
+		Url:         "http://192.168.75.100:9999/plugin/container",
+		// ReverseDest: "http://192.168.48.163:3000/",
+	})
+}
+
+func Client() *splugin.Client {
+	return globalClient
+}
+
+func StartClient(conf *config.HttpConf) {
+	globalClient.Serve(conf.Addr)
+}
---
+++ container/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type HttpConf struct {
+	Addr string `yaml:"addr"`
+}
+
+type MysqlConf struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+}
+
+type ServerConfig struct {
+	Http    *HttpConf    `yaml:"http"`
+	Mysql   *MysqlConf   `yaml:"mysql"`
+}
+
+const config_file = "./config.yml"
+
+var global_config ServerConfig
+
+func Init() {
+	err := readConfig(config_file, &global_config)
+	if err != nil {
+		fmt.Printf("")
+		os.Exit(-1)
+	}
+}
+
+func Config() *ServerConfig {
+	return &global_config
+}
+
+func readConfig(file string, config interface{}) error {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Printf("open %s failed! err = %s\n", file, err.Error())
+		return err
+	}
+
+	err = yaml.Unmarshal(bytes, config)
+	if err != nil {
+		fmt.Printf("yaml Unmarshal %s failed!\n", string(bytes))
+		return err
+	}
+	return nil
+}
