2cf5dc6a6a5213af3e52acaa71201cfad2260a48
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-05-23 15:42:18 +0800

    change import path of grafana plugin

--- grafana/main.go
+++ grafana/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	"openeuler.org/PilotGo/plugin-sdk/plugin"
+	splugin "gitee.com/openeuler/PilotGo-plugins/sdk/plugin"
 )
 
 const Version = "0.0.1"
 
 func main() {
 	fmt.Println("hello grafana")
-	client := plugin.DefaultClient(&plugin.PluginInfo{
+	client := splugin.DefaultClient(&splugin.PluginInfo{
 		Name:        "grafana",
 		Version:     "Version",
 		Description: "grafana可视化工具支持",
