baa2a4dd40899290fd39131077bed9625b0b986a
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-06-07 15:26:43 +0800

    Refactor container plugin based on Grafana plugin

--- container/server/main.go
+++ container/server/main.go
@@ -4,36 +4,58 @@ import (
 	"fmt"
 	"os"
 
+	"gitee.com/openeuler/PilotGo-plugins/sdk/logger"
+	"gitee.com/openeuler/PilotGo-plugins/sdk/plugin/client"
 	"github.com/gin-gonic/gin"
-
-	"openeuler.org/PilotGo/container-plugin/client"
 	"openeuler.org/PilotGo/container-plugin/config"
 	"openeuler.org/PilotGo/container-plugin/database"
 	"openeuler.org/PilotGo/container-plugin/httphandler"
 )
 
-func main() {
-	fmt.Println("hello container")
+const Version = "0.0.1"
 
-	// config.Init()
+var PluginInfo = &client.PluginInfo{
+	Name:        "container",
+	Version:     Version,
+	Description: "Container management plugin",
+	Author:      "wangjunqi",
+	Email:       "wangjunqi@kylinos.cn",
+	Url:         "http://192.168.75.100:9998/plugin/container",
+	// ReverseDest: "",
+}
+
+func main() {
+	fmt.Println("hello gala-ops")
 
 	if err := database.MysqlInit(config.Config().Mysql); err != nil {
 		fmt.Println("failed to initialize database")
 		os.Exit(-1)
 	}
 
-	engine := client.Client().HttpEngine
-	registerHandlers(engine)
-	client.StartClient(config.Config().Http)
+	InitLogger()
+
+	server := gin.Default()
+
+	GlobalClient := client.DefaultClient(PluginInfo)
+	// 临时给server赋值
+	GlobalClient.Server = "http://192.168.75.100:8888"
+	GlobalClient.RegisterHandlers(server)
+	InitRouter(server)
+
+	if err := server.Run(config.Config().Http.Addr); err != nil {
+		logger.Fatal("failed to run server")
+	}
 }
 
-func registerHandlers(engine *gin.Engine) {
-	manager := engine.Group("/plugin_manage")
-	{
-		manager.GET("/info", httphandler.PluginInfo)
+func InitLogger() {
+	if err := logger.Init(config.Config().Logopts); err != nil {
+		fmt.Printf("logger init failed, please check the config file: %s", err)
+		os.Exit(-1)
 	}
-	
-	api := engine.Group("/plugin/container/api")
+}
+
+func InitRouter(router *gin.Engine) {
+	api := router.Group("/plugin/container/api")
 	{
 		api.POST("/deploy_docker", httphandler.DeployDocker)
 	}
