79bfa0b18c22be759c197dc032eada4c3db1373f
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-06-09 13:42:00 +0800

    add httphandler.TargetsList in gala-ops plugin

--- gala-ops/server/httphandler/metricdata.go
+++ gala-ops/server/httphandler/metricdata.go
@@ -1,13 +1,12 @@
 package httphandler
 
 import (
-	"fmt"
-	"time"
+	"encoding/json"
+	"net/http"
 
 	"gitee.com/openeuler/PilotGo-plugins/sdk/logger"
+	"gitee.com/openeuler/PilotGo-plugins/sdk/utils"
 	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/api"
-	prome "github.com/prometheus/client_golang/api/prometheus/v1"
 )
 
 type Plugin struct {
@@ -22,55 +21,18 @@ type Plugin struct {
 	Status      string `json:"status"`
 }
 
-func PrometheusAPI(URL string) (prome.API, error) {
-	// 创建一个HTTP客户端
-	client, err := api.NewClient(api.Config{
-		Address: "http://" + URL,
-	})
-
+func TargetsList(ctx *gin.Context) {
+	// 查询prometheus监控对象列表
+	bs, err := utils.Request("GET", "http://192.168.75.100:8090/plugin/Prometheu/api/v1/query?query=up")
 	if err != nil {
-		return nil, err
+		logger.Error("faild to get prometheus targets: ", err)
 	}
 
-	// 创建一个API客户端
-	promeapi := prome.NewAPI(client)
-	return promeapi, nil
-}
+	var data interface{}
 
-func PrometheusMetrics(ctx *gin.Context, promeclient prome.API) {
-	// bs, err := utils.Request("GET", client.Server+"plugins")
-	// if err != nil {
-	// 	logger.Error("faild to get plugin list: ", err)
-	// }
-	// plugins := &[]*Plugin{}
-	// err = json.Unmarshal(bs, plugins)
-	// if err != nil {
-	// 	logger.Error("unmarshal request plugin info error:%s", err.Error())
-	// }
-	// var Prometheus_addr string
-	// for _, p := range *plugins {
-	// 	if p.Name == "gala-ops" {
-	// 		Prometheus_addr = p.Url
-	// 	}
-	// }
-
-	// 查询所有metrics列表
-	result, warnings, err := promeclient.Query(ctx, "up", time.Now())
-
+	err = json.Unmarshal(bs, &data)
 	if err != nil {
-		logger.Error("failed to query prometheus: ", err)
-		return
+		logger.Error("unmarshal prometheus targets error:%s", err.Error())
 	}
-
-	if len(warnings) > 0 {
-		logger.Warn("Warnings:", warnings)
-		return
-	}
-
-	fmt.Println("Metrics:")
-	fmt.Println(result)
-	// for _, metric := range result {
-	// 	fmt.Println(metric)
-	// }
-
+	ctx.JSON(http.StatusOK, data)
 }
