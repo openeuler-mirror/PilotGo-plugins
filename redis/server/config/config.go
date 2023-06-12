1925ce594e9c8a21e62d618ff0e29df5753e9e2d
wubijie <wubijie@kylinos.cn>
2023-06-12 11:45:25 +0800

    Initialize the redis mod and add installation functionality

---
+++ redis/server/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gitee.com/openeuler/PilotGo-plugins/sdk/logger"
+	"gopkg.in/yaml.v2"
+)
+
+type Redis struct {
+	URL         string `yaml:"url"`
+	ReverseDest string `yaml:"reverseDest"`
+}
+
+type HttpConf struct {
+	Addr string `yaml:"addr"`
+}
+
+// redis的密码怎么添加至配置文件？
+type ServerConfig struct {
+	Redis   *Redis          `yaml:"redis"`
+	Http    *HttpConf       `yaml:"http"`
+	Logopts *logger.LogOpts `yaml:"log"`
+}
+
+const config_file = "./config.yml"
+
+var global_config ServerConfig
+
+func Init() {
+	err := readConfig(config_file, &global_config)
+	if err != nil {
+		fmt.Printf("%v", err.Error())
+		os.Exit(-1)
+	}
+}
+
+func Config() *ServerConfig {
+	return &global_config
+}
+
+func readConfig(file string, config interface{}) error {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Printf("open %s failed! err = %s\n", file, err.Error())
+		return err
+	}
+
+	err = yaml.Unmarshal(bytes, config)
+	if err != nil {
+		fmt.Printf("yaml Unmarshal %s failed!\n", string(bytes))
+		return err
+	}
+	return nil
+}
