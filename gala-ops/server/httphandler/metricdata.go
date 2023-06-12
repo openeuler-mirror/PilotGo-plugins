c201cfa67b689863fafb0d6039f868477275ff73
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-06-12 13:34:45 +0800

    add opsclient in gala-ops plugin

--- gala-ops/server/httphandler/metricdata.go
+++ gala-ops/server/httphandler/metricdata.go
@@ -9,21 +9,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Plugin struct {
-	UUID        string `json:"uuid"`
-	Name        string `json:"name"`
-	Version     string `json:"version"`
-	Description string `json:"description"`
-	Author      string `json:"author"`
-	Email       string `json:"email"`
-	Url         string `json:"url"`
-	Enabled     int    `json:"enabled"`
-	Status      string `json:"status"`
-}
-
 func TargetsList(ctx *gin.Context) {
 	// 查询prometheus监控对象列表
-	bs, err := utils.Request("GET", "http://192.168.75.100:8090/plugin/Prometheu/api/v1/query?query=up")
+	bs, err := utils.Request("GET", Galaops.PromePlugin["url"].(string)+"/api/v1/query?query=up")
 	if err != nil {
 		logger.Error("faild to get prometheus targets: ", err)
 	}
