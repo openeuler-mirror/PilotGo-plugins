d98f41a5844690aba37e82a862c740944de9b628
wubijie <wubijie@kylinos.cn>
2023-06-17 16:12:17 +0800

    Split the file

--- redis/server/httphandler/agent.go
+++ redis/server/httphandler/agent.go
@@ -1,65 +1,32 @@
 package httphandler
 
 import (
-	"fmt"
 	"net/http"
 
+	"gitee.com/openeuler/PilotGo-plugins/sdk/common"
 	"github.com/gin-gonic/gin"
-	"openeuler.org/PilotGo/redis-plugin/global"
-	"openeuler.org/PilotGo/redis-plugin/plugin"
+	"openeuler.org/PilotGo/redis-plugin/service"
 )
 
 // 安装运行
 func InstallRedisExporter(ctx *gin.Context) {
-	// TODO
-	param := &struct {
-		Batch []string
-	}{}
+	// TODOs
+	var param *common.Batch
+
 	if err := ctx.BindJSON(param); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"code":   -1,
-			"status": "redis",
+			"status": err,
 		})
 	}
 
-	cmd := "yum install -y redis_exporter && systemctl start redis_exporter"
-	cmdResults, err := global.GlobalClient.RunScript(param.Batch, cmd)
+	ret, err := service.Install(param)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"code":   -1,
-			"status": fmt.Sprintf("run redis exporter error:%s", err),
+			"status": err,
 		})
 	}
-	ret := []interface{}{}
-	monitorTargets := []string{}
-	for _, result := range cmdResults {
-		d := struct {
-			MachineUUID   string
-			MachineIP     string
-			InstallStatus string
-			Error         string
-		}{
-			MachineUUID:   result.MachineUUID,
-			InstallStatus: "ok",
-			Error:         "",
-		}
-
-		if result.Code != 0 {
-			d.InstallStatus = "error"
-			d.Error = result.Stderr
-		} else {
-			// TODO: add redis exporter to prometheus monitor target here
-			// default exporter port :9121
-			monitorTargets = append(monitorTargets, result.MachineIP+":9121")
-		}
-
-		ret = append(ret, d)
-	}
-	err = plugin.MonitorTargets(monitorTargets)
-	if err != nil {
-		fmt.Println("error: failed to add redis exporter to prometheus monitor targets")
-	}
-
 	ctx.JSON(http.StatusOK, gin.H{
 		"code":   0,
 		"status": "ok",
