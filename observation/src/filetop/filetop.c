c9f2e19648b8baa2db7bac49c2559b5aad284939
jxy_git <jiangxinyu@kylinos.cn>
2023-06-27 16:10:31 +0800

    Defines the command line parameters supported by the program

--- observation/src/filetop/filetop.c
+++ observation/src/filetop/filetop.c
@@ -39,3 +39,14 @@ const char argp_program_doc[] =
 "    filetop            # file I/O top, refresh every 1s\n"
 "    filetop -p 1216    # only trace PID 1216\n"
 "    filetop 5 10       # 5s summaries, 10 times\n";
+
+static const struct argp_option opts[] = {
+	{ "pid", 'p', "PID", 0, "Process ID to trace" },
+	{ "noclear", 'C', NULL, 0, "Don't clear the screen" },
+	{ "all", 'a', NULL, 0, "Include special files" },
+	{ "sort", 's', "SORT", 0, "Sort columns, default all [all, reads, writes, rbytes, wbytes]" },
+	{ "rows", 'r', "ROWS", 0, "Maximum rows to print, default 20" },
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{}
+};
