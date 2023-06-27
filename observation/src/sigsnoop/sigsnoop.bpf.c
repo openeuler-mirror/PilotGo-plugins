9c4c94d8db93699798175001ca13ea7f41cc76bd
li-long315 <lilong@kylinos.cn>
2023-06-28 04:03:22 +0800

    Set the entry point function of the BPF program to handle different tracking events

--- observation/src/sigsnoop/sigsnoop.bpf.c
+++ observation/src/sigsnoop/sigsnoop.bpf.c
@@ -70,3 +70,83 @@ static __always_inline int probe_exit(void *ctx, int ret)
 	submit_buf(ctx, buf, sizeof(*buf));
 	return 0;
 }
+
+SEC("tracepoint/syscalls/sys_enter_kill")
+int kill_entry(struct trace_event_raw_sys_enter *ctx)
+{
+	pid_t tpid = (pid_t)ctx->args[0];
+	int sig = (int)ctx->args[1];
+
+	return probe_entry(tpid, sig);
+}
+
+SEC("tracepoint/syscalls/sys_exit_kill")
+int kill_exit(struct trace_event_raw_sys_exit *ctx)
+{
+	return probe_exit(ctx, ctx->ret);
+}
+
+SEC("tracepoint/syscalls/sys_enter_tkill")
+int tkill_entry(struct trace_event_raw_sys_enter *ctx)
+{
+	pid_t tpid = (pid_t)ctx->args[0];
+	int sig = (int)ctx->args[1];
+
+	return probe_entry(tpid, sig);
+}
+
+SEC("tracepoint/syscalls/sys_exit_tkill")
+int tkill_exit(struct trace_event_raw_sys_exit *ctx)
+{
+	return probe_exit(ctx, ctx->ret);
+}
+
+SEC("tracepoint/syscalls/sys_enter_tgkill")
+int tgkill_entry(struct trace_event_raw_sys_enter *ctx)
+{
+	pid_t tpid = (pid_t)ctx->args[1];
+	int sig = (int)ctx->args[2];
+
+	return probe_entry(tpid, sig);
+}
+
+SEC("tracepoint/syscalls/sys_exit_tgkill")
+int tgkill_exit(struct trace_event_raw_sys_exit *ctx)
+{
+	return probe_exit(ctx, ctx->ret);
+}
+
+SEC("tracepoint/signal/signal_generate")
+int sig_trace(struct trace_event_raw_signal_generate *ctx)
+{
+	struct event *eventp = NULL;
+	pid_t tpid = ctx->pid;
+	int ret = ctx->errno;
+	int sig = ctx->sig;
+	__u32 tgid;
+
+	if (failed_only && ret == 0)
+		return 0;
+
+	if (target_signal && sig != target_signal)
+		return 0;
+
+	tgid = bpf_get_current_pid_tgid() >> 32;
+	if (filtered_pid && tgid != filtered_pid)
+		return 0;
+
+	eventp = reserve_buf(sizeof(*eventp));
+	if (!eventp)
+		return 0;
+
+	eventp->pid = tgid;
+	eventp->tpid = tpid;
+	eventp->sig = sig;
+	eventp->ret = -ret;
+	bpf_get_current_comm(&eventp->comm, sizeof(eventp->comm));
+	submit_buf(ctx, eventp, sizeof(*eventp));
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
