8ee6b5165bf0e79ac7c79e0668f76a138b6db613
li-long315 <lilong@kylinos.cn>
2023-06-27 08:57:42 +0800

    Define relevant structures counts start stackmap

--- observation/src/wakeuptime/wakeuptime.bpf.c
+++ observation/src/wakeuptime/wakeuptime.bpf.c
@@ -8,7 +8,30 @@
 
 #define PF_KTHREAD	0x00200000 /* kernel thread */
 
-SEC("tp_btf/sched_switch")
+const volatile pid_t target_pid = 0;
+const volatile __u64 max_block_ns = -1;
+const volatile __u64 min_block_ns = 1;
+const volatile bool user_threads_only = false;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, struct key_t);
+	__type(value, u64);
+} counts SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, u32);
+	__type(value, u64);
+} start SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_STACK_TRACE);
+	__uint(key_size, sizeof(u32));
+} stackmap SEC(".maps");
+
 int BPF_PROG(sched_switch_btf, bool preempt, struct task_struct *prev,
 	     struct task_struct *next)
 {
