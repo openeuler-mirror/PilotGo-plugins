806d961aaed3ff389dc6f2484a5d1ada3e708edc
li-long315 <lilong@kylinos.cn>
2023-06-27 21:34:40 +0800

    Set signal processing functions and loop processing events

--- observation/src/sigsnoop/sigsnoop.c
+++ observation/src/sigsnoop/sigsnoop.c
@@ -15,6 +15,41 @@ static bool kill_only = false;
 static bool signal_name = false;
 static bool verbose = false;
 
+static const char *sig_name[] = {
+	[0] = "N/A",
+	[1] = "SIGHUP",
+	[2] = "SIGINT",
+	[3] = "SIGQUIT",
+	[4] = "SIGILL",
+	[5] = "SIGTRAP",
+	[6] = "SIGABRT",
+	[7] = "SIGSIGBUS",
+	[8] = "SIGFPE",
+	[9] = "SIGKILL",
+	[10] = "SIGUSR1",
+	[11] = "SIGSEGV",
+	[12] = "SIGUSR2",
+	[13] = "SIGPIPE",
+	[14] = "SIGALRM",
+	[15] = "SIGTERM",
+	[16] = "SIGSTKFLT",
+	[17] = "SIGCHLD",
+	[18] = "SIGCONT",
+	[19] = "SIGSTOP",
+	[20] = "SIGTSTP",
+	[21] = "SIGTTIN",
+	[22] = "SIGTTOU",
+	[23] = "SIGURG",
+	[24] = "SIGXCPU",
+	[25] = "SIGXFSZ",
+	[26] = "SIGVTALRM",
+	[27] = "SIGPROF",
+	[28] = "SIGWINCH",
+	[29] = "SIGIO",
+	[30] = "SIGPWR",
+	[31] = "SIGSYS",
+};
+
 const char *argp_program_version = "sigsnoop 0.1";
 const char *argp_program_bug_address = "Jackie Liu <liuyun01@kylinos.cn>";
 const char argp_program_doc[] =
@@ -86,6 +121,11 @@ static void alias_parse(char *prog)
 		kill_only = true;
 }
 
+static void sig_handler(int sig)
+{
+	exiting = 1;
+}
+
 static int handle_event(void *ctx, void *data, size_t data_sz)
 {
 	struct event *e = data;
@@ -125,19 +165,19 @@ int main(int argc, char *argv[])
 	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
 	if (err)
 		return err;
-        
+
 	if (!bpf_is_root())
 		return 1;
 
-	libbpf_set_print(libbpf_print_fn);	
-        
-        obj = sigsnoop_bpf__open();
+	libbpf_set_print(libbpf_print_fn);
+
+	obj = sigsnoop_bpf__open();
 	if (!obj) {
 		warning("Failed to open BPF object\n");
 		return 1;
-	}	
+	}
 
-        obj->rodata->filtered_pid = target_pid;
+	obj->rodata->filtered_pid = target_pid;
 	obj->rodata->target_signal = target_signal;
 	obj->rodata->failed_only = failed_only;
 
@@ -152,7 +192,7 @@ int main(int argc, char *argv[])
 		bpf_program__set_autoload(obj->progs.tgkill_exit, false);
 	}
 
-        buf = bpf_buffer__new(obj->maps.events, obj->maps.heap);
+	buf = bpf_buffer__new(obj->maps.events, obj->maps.heap);
 	if (!buf) {
 		err = -errno;
 		warning("Failed to create ring/perf buffer: %d\n", err);
@@ -176,6 +216,29 @@ int main(int argc, char *argv[])
 		warning("Failed to open ring/perf buffer: %d\n", err);
 		goto cleanup;
 	}
+
+	if (signal(SIGINT, sig_handler) == SIG_ERR) {
+		err = 1;
+		warning("Can't set signal handler: %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	printf("%-8s %-7s %-16s %-9s %-7s %6s\n",
+	       "TIME", "PID", "COMM", "SIG", "TPID", "RESULT");
+
+	while (!exiting) {
+		err = bpf_buffer__poll(buf, POLL_TIMEOUT_MS);
+		if (err < 0 && err != -EINTR) {
+			warning("Error polling ring/perf buffer: %d\n", err);
+			goto cleanup;
+		}
+		/* reset err to return 0 if exiting */
+		err = 0;
+	}
+
+cleanup:
+	bpf_buffer__free(buf);
+	sigsnoop_bpf__destroy(obj);
 
 	return err != 0;
 }
