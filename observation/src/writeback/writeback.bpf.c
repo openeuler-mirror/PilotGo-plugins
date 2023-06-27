d9fa45381783c07f7a7502bef70c38be4b08431e
li-long315 <lilong@kylinos.cn>
2023-06-27 19:59:02 +0800

    Add writeback_written event handling functions

--- observation/src/writeback/writeback.bpf.c
+++ observation/src/writeback/writeback.bpf.c
@@ -27,3 +27,37 @@ int tracepoint_writeback_start(struct trace_event_raw_writeback_work_class *ctx)
 	return 0;
 }
 
+SEC("tracepoint/writeback/writeback_written")
+int tracepoint_writeback_written(struct trace_event_raw_writeback_work_class *ctx)
+{
+	struct event *event;
+
+	long nr_pages = ctx->nr_pages;
+	dev_t sb_dev = ctx->sb_dev;
+	int reason = ctx->reason;
+	__s64 latency;
+
+	__u64 *start = bpf_map_lookup_and_delete_elem(&birth, &sb_dev);
+	if (!start)
+		return 0;
+
+	event = reserve_buf(sizeof(*event));
+	if (!event)
+		return 0;
+
+	BPF_CORE_READ_STR_INTO(&event->name, ctx, name);
+	event->nr_pages = nr_pages;
+	event->reason = reason;
+
+	latency = bpf_ktime_get_ns() - *start;
+	if (latency < 0)
+		latency = 0;
+
+	event->latency = latency / 1000;
+	submit_buf(ctx, event, sizeof(*event));
+
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
+
