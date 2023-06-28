f09179d9b8905df06530d80236bf5238ba24817a
zhanghan2021 <zhanghan@kylinos.cn>
2023-06-28 17:28:49 +0800

    add probe schedule switch func in offcputime

--- observation/src/offcputime/offcputime.bpf.c
+++ observation/src/offcputime/offcputime.bpf.c
@@ -59,3 +59,56 @@ static bool allow_record(struct task_struct *task)
         return false;
     return true;
 }
+
+static __always_inline int probe_sched_switch(void *ctx, bool preempt, struct task_struct *prev,
+                                              struct task_struct *next)
+{
+    struct internal_key *i_keyp, i_key;
+    offcpu_val_t *valp, val;
+    s64 delta;
+    u32 pid;
+
+    if (allow_record(prev))
+    {
+        pid = BPF_CORE_READ(prev, pid);
+        /* To distinguish idle threads of different cores */
+        if (!pid)
+            pid = bpf_get_smp_processor_id();
+
+        i_key.key.pid = pid;
+        i_key.key.tgid = BPF_CORE_READ(prev, tgid);
+        i_key.start_ts = bpf_ktime_get_ns();
+
+        if (BPF_CORE_READ(prev, flags) & PF_KTHREAD)
+            i_key.key.user_stack_id = -1;
+        else
+            i_key.key.user_stack_id =
+                bpf_get_stackid(ctx, &stackmap, BPF_F_USER_STACK);
+        i_key.key.kernel_stack_id = bpf_get_stackid(ctx, &stackmap, 0);
+        bpf_map_update_elem(&start, &pid, &i_key, BPF_ANY);
+        BPF_CORE_READ_STR_INTO(&val.comm, prev, comm);
+        val.delta = 0;
+        bpf_map_update_elem(&info, &i_key.key, &val, BPF_NOEXIST);
+    }
+
+    pid = BPF_CORE_READ(next, pid);
+    i_keyp = bpf_map_lookup_elem(&start, &pid);
+    if (!i_keyp)
+        return 0;
+
+    delta = (s64)(bpf_ktime_get_ns() - i_keyp->start_ts);
+    if (delta < 0)
+        goto cleanup;
+    delta /= 1000U;
+    if (delta < min_block_ns || delta > max_block_ns)
+        goto cleanup;
+    valp = bpf_map_lookup_elem(&info, &i_keyp->key);
+    if (!valp)
+        goto cleanup;
+    __sync_fetch_and_add(&valp->delta, delta);
+
+cleanup:
+    bpf_map_delete_elem(&start, &pid);
+
+    return 0;
+}
