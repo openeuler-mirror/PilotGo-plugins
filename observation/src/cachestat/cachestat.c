7c717c498a72ed82436a5b4828fb3dc73070b4b6
wubijie <wubijie@kylinos.cn>
2023-06-28 14:07:28 +0800

    Increase the overall monitoring process of cachestat probes

--- observation/src/cachestat/cachestat.c
+++ observation/src/cachestat/cachestat.c
@@ -106,3 +106,158 @@ static int get_meminfo(__u64 *buffers, __u64 *cached)
 	fclose(f);
 	return 0;
 }
+
+int main(int argc, char *argv[])
+{
+	static const struct argp argp = {
+		.options = opts,
+		.parser = parse_arg,
+		.doc = argp_program_doc,
+	};
+	struct argument argument = {
+		.interval = -1,
+		.times = 99999999,
+	};
+	__u64 buffers, cached, mbd;
+	struct cachestat_bpf *obj;
+	__s64 total, misses, hits;
+	int err;
+
+	err = argp_parse(&argp, argc, argv, 0, NULL, &argument);
+	if (err)
+		return err;
+
+	if (!bpf_is_root())
+		return 1;
+
+	libbpf_set_print(libbpf_print_fn);
+
+	obj = cachestat_bpf__open();
+	if (!obj) {
+		warning("Failed to open BPF object\n");
+		return 1;
+	}
+
+	/*
+	 * account_page_dirtied was renamed to folio_account_dirtied
+	 * in kernel commit 203a31516616 ("mm/writeback: Add __folio_mark_dirty()")
+	 */
+	if (tracepoint_exists("writeback", "writeback_dirty_folio")) {
+		bpf_program__set_autoload(obj->progs.tracepoint_writeback_dirty_page, false);
+	} else if (tracepoint_exists("writeback", "writeback_dirty_page")) {
+		bpf_program__set_autoload(obj->progs.tracepoint_writeback_dirty_folio, false);
+	} else {
+		warning("dirty entry not found in current kernel, please upgrade.\n");
+		goto cleanup;
+	}
+
+	/* misses add tracepoint */
+	if (fentry_can_attach("filemap_add_folio", NULL)) {
+		err = bpf_program__set_attach_target(obj->progs.fentry_add_to_page_cache_lru, 0,
+						     "filemap_add_folio");
+		if (err) {
+			warning("Failed to set attach target\n");
+			goto cleanup;
+		}
+	}
+
+	if (fentry_can_attach("add_to_page_cache_lru", NULL) ||
+	    fentry_can_attach("filemap_add_folio", NULL)) {
+		bpf_program__set_autoload(obj->progs.kprobe_add_to_page_cache_lru, false);
+		bpf_program__set_autoload(obj->progs.kprobe_filemap_add_folio, false);
+	} else {
+		if (kprobe_exists("filemap_add_folio"))
+			bpf_program__set_autoload(obj->progs.kprobe_add_to_page_cache_lru, false);
+		else
+			bpf_program__set_autoload(obj->progs.kprobe_filemap_add_folio, false);
+		bpf_program__set_autoload(obj->progs.fentry_add_to_page_cache_lru, false);
+	}
+
+	/* total add tracepoint */
+	if (fentry_can_attach("mark_page_accessed", NULL))
+		bpf_program__set_autoload(obj->progs.kprobe_mark_page_accessed, false);
+	else
+		bpf_program__set_autoload(obj->progs.fentry_mark_page_accessed, false);
+
+	if (fentry_can_attach("mark_buffer_dirty", NULL))
+		bpf_program__set_autoload(obj->progs.kprobe_mark_buffer_dirty, false);
+	else
+		bpf_program__set_autoload(obj->progs.fentry_mark_buffer_dirty, false);
+
+	err = cachestat_bpf__load(obj);
+	if (err) {
+		warning("Failed to load BPF object\n");
+		goto cleanup;
+	}
+
+	if (!obj->bss) {
+		warning("Memory-mapping BPF maps is supported starting from Linux 5.7, please upgrade.\n");
+		goto cleanup;
+	}
+
+	err = cachestat_bpf__attach(obj);
+	if (err) {
+		warning("Failed to attach BPF programs\n");
+		goto cleanup;
+	}
+
+	signal(SIGINT, sig_handler);
+
+	if (argument.timestamp)
+		printf("%-8s ", "TIME");
+	printf("%8s %8s %8s %8s %12s %10s\n", "HITS", "MISSES", "DIRTIES",
+		"HITRATIO", "BUFFERS_MB", "CACHED_MB");
+
+	while (1) {
+		float ratio;
+
+		sleep(argument.interval);
+
+		/* total = total cache accesses without counting dirties */
+		total = __atomic_exchange_n(&obj->bss->total, 0, __ATOMIC_RELAXED);
+		/* misses = total of add to lru because of read misses */
+		misses = __atomic_exchange_n(&obj->bss->misses, 0, __ATOMIC_RELAXED);
+		/* mbd = total of mark_buffer_dirty events */
+		mbd = __atomic_exchange_n(&obj->bss->mbd, 0, __ATOMIC_RELAXED);
+
+		if (total < 0)
+			total = 0;
+		if (misses < 0)
+			misses = 0;
+		hits = total - misses;
+
+		/*
+		 * If hits are < 0, then its possible misses are overestimated
+		 * due to possibly page cache read ahead adding more pages than
+		 * needed. In this case just assume misses as total and reset
+		 * hits.
+		 */
+		if (hits < 0) {
+			misses = total;
+			hits = 0;
+		}
+		ratio = total > 0 ? hits * 1.0 / total : 0.0;
+		err = get_meminfo(&buffers, &cached);
+		if (err) {
+			warning("Failed to get meminfo: %d\n", err);
+			goto cleanup;
+		}
+
+		if (argument.timestamp) {
+			char ts[32];
+
+			strftime_now(ts, sizeof(ts), "%H:%M:%S");
+			printf("%-8s ", ts);
+		}
+		printf("%8lld %8lld %8llu %7.2f%% %12llu %10llu\n",
+			hits, misses, mbd, 100 * ratio,
+			buffers / 1024, cached / 1024);
+
+		if (exiting || --argument.times == 0)
+			break;
+	}
+
+cleanup:
+	cachestat_bpf__destroy(obj);
+	return err != 0;
+}
