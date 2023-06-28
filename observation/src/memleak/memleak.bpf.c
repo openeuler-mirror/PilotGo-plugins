15ff700fdedf7c609e7299b9ac5c83a258df2f3f
zhanghan2021 <zhanghan@kylinos.cn>
2023-06-28 16:48:14 +0800

    define consts  for memleak

---
+++ observation/src/memleak/memleak.bpf.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_helpers.h>
+
+#include "maps.bpf.h"
+#include "core_fixes.bpf.h"
+#include "memleak.h"
+
+const volatile size_t min_size = 0;
+const volatile size_t max_size = -1;
+const volatile size_t page_size = 4096;
+const volatile __u64 sample_rate = 1;
+const volatile bool trace_all = false;
+const volatile __u64 stack_flags = 0;
+const volatile bool wa_missing_free = false;
