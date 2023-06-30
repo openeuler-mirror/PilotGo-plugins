c390cbed2e82cf528fa0a68b9a5f4a2a6184520c
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-06-30 14:05:16 +0800

    hardirqs probe: define sig_atomic_t、argp_program_version、argp_program_bug_address and argp_program_doc

--- observation/src/hardirqs/hardirqs.c
+++ observation/src/hardirqs/hardirqs.c
@@ -18,3 +18,19 @@ struct env {
 	.interval = 99999999,
 	.times = 99999999,
 };
+
+static volatile sig_atomic_t exiting;
+
+const char *argp_program_version = "hardirqs 0.1";
+const char *argp_program_bug_address = "Jackie Liu <liuyun01@kylinos.cn>";
+const char argp_program_doc[] =
+"Summarize hard irq event time as histograms.\n"
+"\n"
+"USAGE: hardirqs [--help] [-T] [-N] [-d] [interval] [count] [-c CG]\n"
+"\n"
+"EXAMPLES:\n"
+"    hardirqs            # sum hard irq event time\n"
+"    hardirqs -d         # show hard irq event time as histograms\n"
+"    hardirqs 1 10       # print 1 second summaries, 10 times\n"
+"    hardirqs -c CG      # Trace process under cgroupsPath CG\n"
+"    hardirqs -NT 1      # 1s summaries, nanoseconds, and timestamps\n";
