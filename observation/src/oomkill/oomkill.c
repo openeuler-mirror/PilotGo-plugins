a7fdbc23e416211117c70e78f806b9a60a13ed47
zhanghan2021 <zhanghan@kylinos.cn>
2023-06-30 14:27:41 +0800

    libbpf print handle in oomkill

--- observation/src/oomkill/oomkill.c
+++ observation/src/oomkill/oomkill.c
@@ -75,3 +75,11 @@ static void sig_handler(int sig)
 {
     exiting = 1;
 }
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+                           va_list args)
+{
+    if (level == LIBBPF_DEBUG && !verbose)
+        return 0;
+    return vfprintf(stderr, format, args);
+}
