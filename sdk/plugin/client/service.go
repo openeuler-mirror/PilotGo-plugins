467aa1ea1af1dd1bb8d2db607a2b8a880b48f1ed
wubijie <wubijie@kylinos.cn>
2023-07-11 15:22:29 +0800

    add service defines of sdk

---
+++ sdk/plugin/client/service.go
@@ -0,0 +1,13 @@
+package client
+
+import "gitee.com/openeuler/PilotGo-plugins/sdk/common"
+
+func (c *Client) StartService(batch *common.Batch, cmd string) ([]*CmdResult, error) {
+
+	return nil, nil
+}
+
+func (c *Client) StopService(batch *common.Batch, cmd string) ([]*CmdResult, error) {
+
+	return nil, nil
+}
