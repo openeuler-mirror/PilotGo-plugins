f7b34c6667fbb8ab00f538652a4508edbd0c1912
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-08-15 11:40:32 +0800

    initialize topology plugin

---
+++ topology/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gitee.com/openeuler/PilotGo-plugin-topology-server/clientmanager"
+	"gitee.com/openeuler/PilotGo-plugin-topology-server/conf"
+	"gitee.com/openeuler/PilotGo-plugin-topology-server/router"
+	"gitee.com/openeuler/PilotGo-plugins/sdk/logger"
+	"gitee.com/openeuler/PilotGo-plugins/sdk/plugin/client"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("hello topology")
+
+	InitLogger()
+
+	// TODO: init arangodb
+
+	PluginClient := client.DefaultClient(clientmanager.PluginInfo)
+	// 临时给server赋值
+	PluginClient.Server = "http://192.168.75.100:8887"
+	clientmanager.Galaops = &clientmanager.Opsclient{
+		Sdkmethod: PluginClient,
+	}
+
+	// 设置router
+	engine := gin.Default()
+	clientmanager.Galaops.Sdkmethod.RegisterHandlers(engine)
+	router.InitRouter(engine)
+	if err := engine.Run(conf.Config().Http.Addr); err != nil {
+		logger.Fatal("failed to run server")
+	}
+}
+
+func InitLogger() {
+	if err := logger.Init(conf.Config().Logopts); err != nil {
+		fmt.Printf("logger init failed, please check the config file: %s", err)
+		os.Exit(1)
+	}
+}
