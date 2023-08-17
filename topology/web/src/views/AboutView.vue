720fc08b10bbe3a75de7128e1fd18ad46b1b319a
Gzx1999 <guozhengxin@kylinos.cn>
2023-08-17 18:56:01 +0800

    update topo page layout

--- topology/web/src/views/AboutView.vue
+++ topology/web/src/views/AboutView.vue
@@ -1,15 +1,82 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="h1">拓扑图演示页面</h1>
+    <div id="topo-container" class="container" ref="to"></div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script setup lang="ts">
+import G6 from '@antv/g6';
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const data = {
+    canvasWidth:0,
+    canvasHeight:0,
+
+    // 节点
+    nodes: [
+      {
+        id: 'node1',
+        x: 100,
+        y: 200,
+      },
+      {
+        id: 'node2',
+        x: 300,
+        y: 200,
+      },
+    ],
+    // 边集
+    edges: [
+      // 表示一条从 node1 节点连接到 node2 节点的边
+      {
+        source: 'node1',
+        target: 'node2',
+      },
+    ],
+  };
+
+  let width = document.getElementById("topo-container")!.clientWidth;
+  let height = document.getElementById("topo-container")!.clientHeight;
+  console.log("canvas width: " + width + " height: " + height)
+
+  const graph = new G6.Graph({
+    container: "topo-container",
+    width: width,
+    height: height,
+  });
+  graph.data(data);
+  graph.render();
+
+  window.onresize = ()=> {
+    let width = document.getElementById("topo-container")!.clientWidth;
+    let height = document.getElementById("topo-container")!.clientHeight;
+    graph.changeSize(width, height)
+    graph.fitCenter()
+    console.log("resize: " + width + ","+ height);
   }
+
+})
+
+</script>
+
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+}
+
+.h1 {
+  width: 100%;
+  text-align: center;
+}
+
+.container {
+  flex-grow: 1;
+  background-color: white;
 }
 </style>
