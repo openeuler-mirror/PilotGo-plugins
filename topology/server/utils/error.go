04305cee7fa6d8d13a677f47fab0945d479cd418
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-09-02 17:38:11 +0800

    fix initmachinelist() bug; add build file

--- topology/server/utils/error.go
+++ topology/server/utils/error.go
@@ -2,12 +2,12 @@ package utils
 
 import "runtime"
 
-func CallerInfo(err error) (string, int, string) {
-	if err != nil {
-		pro_c, filepath, line, ok := runtime.Caller(1)
-		if ok {
-			return filepath, line - 2, runtime.FuncForPC(pro_c).Name()
-		}
+func CallerInfo() (string, int, string) {
+	// if err != nil {
+	pro_c, filepath, line, ok := runtime.Caller(1)
+	if ok {
+		return filepath, line - 2, runtime.FuncForPC(pro_c).Name()
 	}
+	// }
 	return "", -1, ""
 }
