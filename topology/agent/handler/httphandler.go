e6768c352a51374e09939e1fef2890f446a8f60d
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-09-05 15:53:36 +0800

    modify getcollectdatafromtopoagent()

--- topology/agent/handler/httphandler.go
+++ topology/agent/handler/httphandler.go
@@ -14,14 +14,15 @@ func Raw_metric_data(ctx *gin.Context) {
 	if err != nil {
 		logger.Error("(Raw_metric_data->DataCollectorService: %s)", err)
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code":   -1,
-			"status": fmt.Errorf("(Raw_metric_data->DataCollectorService: %s)", err),
+			"code":  -1,
+			"error": fmt.Errorf("(Raw_metric_data->DataCollectorService: %s)", err),
+			"data":  nil,
 		})
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"code":   0,
-		"status": "ok",
-		"data":   data,
+		"code":  0,
+		"error": nil,
+		"data":  data,
 	})
 }
