7107fc627bf9e14e6d6da0cf37106b3f2950e39e
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-09-09 12:31:11 +0800

    fix topology plugin bug

--- topology/server/service/service.go
+++ topology/server/service/service.go
@@ -10,13 +10,39 @@ func DataProcessService() ([]*meta.Node, []*meta.Edge, []error, []error) {
 	dataprocesser := processor.CreateDataProcesser()
 	nodes, edges, collect_errlist, process_errlist := dataprocesser.Process_data()
 
-	for i, cerr := range collect_errlist {
-		collect_errlist[i] = errors.Wrap(cerr, "**3")
+	if len(collect_errlist) != 0 || len(process_errlist) != 0 {
+		for i, cerr := range collect_errlist {
+			collect_errlist[i] = errors.Wrap(cerr, "**3")
+		}
+
+		for i, perr := range process_errlist {
+			process_errlist[i] = errors.Wrap(perr, "**7")
+		}
 	}
 
-	for i, perr := range process_errlist {
-		process_errlist[i] = errors.Wrap(perr, "**7")
-	}
+	// if len(collect_errlist) != 0 && len(process_errlist) != 0 {
+	// 	for i, cerr := range collect_errlist {
+	// 		collect_errlist[i] = errors.Wrap(cerr, "**3")
+	// 	}
+
+	// 	for i, perr := range process_errlist {
+	// 		process_errlist[i] = errors.Wrap(perr, "**7")
+	// 	}
+
+	// 	return nil, nil, collect_errlist, process_errlist
+	// } else if len(collect_errlist) != 0 && len(process_errlist) == 0 {
+	// 	for i, cerr := range collect_errlist {
+	// 		collect_errlist[i] = errors.Wrap(cerr, "**3")
+	// 	}
+
+	// 	return nil, nil, collect_errlist, nil
+	// } else if len(collect_errlist) == 0 && len(process_errlist) != 0 {
+	// 	for i, perr := range process_errlist {
+	// 		process_errlist[i] = errors.Wrap(perr, "**7")
+	// 	}
+
+	// 	return nil, nil, nil, process_errlist
+	// }
 
 	return nodes.Nodes, edges.Edges, collect_errlist, process_errlist
 }
