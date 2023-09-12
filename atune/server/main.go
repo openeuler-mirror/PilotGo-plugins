30ee31657c51d1617e004d5e0081d901ebad5909
zhanghan2021 <zhanghan@kylinos.cn>
2023-09-12 08:59:48 +0800

    add http router for atune plugin

---
+++ atune/server/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"gitee.com/openeuler/PilotGo-plugins/sdk/logger"
+	"gitee.com/openeuler/PilotGo-plugins/sdk/plugin/client"
+	"openeuler.org/PilotGo/atune-plugin/config"
+	"openeuler.org/PilotGo/atune-plugin/db"
+	"openeuler.org/PilotGo/atune-plugin/plugin"
+	"openeuler.org/PilotGo/atune-plugin/router"
+)
+
+func main() {
+	fmt.Println("hello atune")
+
+	config.Init()
+
+	if err := logger.Init(config.Config().Logopts); err != nil {
+		fmt.Printf("logger init failed, please check the config file: %s", err)
+		os.Exit(-1)
+	}
+
+	if err := db.MysqldbInit(config.Config().Mysql); err != nil {
+		logger.Error("mysql db init failed, please check again: %s", err)
+		os.Exit(-1)
+	}
+
+	err := router.HttpServerInit(config.Config().HttpServer)
+	if err != nil {
+		logger.Error("http server init failed, error:%v", err)
+		os.Exit(-1)
+	}
+
+	plugin.GlobalClient = client.DefaultClient(plugin.Init(config.Config().PluginAtune))
+	plugin.GlobalClient.Server = config.Config().HttpServer.Addr
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	for {
+		s := <-c
+		switch s {
+		case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			logger.Info("signal interrupted: %s", s.String())
+			goto EXIT
+		default:
+			logger.Info("unknown signal: %s", s.String())
+		}
+	}
+
+EXIT:
+	logger.Info("exit system, bye~")
+}
---
+++ atune/server/router/router.go
@@ -0,0 +1,44 @@
+package router
+
+import (
+	"gitee.com/openeuler/PilotGo-plugins/sdk/logger"
+	"github.com/gin-gonic/gin"
+	"openeuler.org/PilotGo/atune-plugin/config"
+	"openeuler.org/PilotGo/atune-plugin/plugin"
+)
+
+func HttpServerInit(conf *config.HttpServer) error {
+
+	go func() {
+		r := setupRouter()
+
+		logger.Info("start http service on: http://%s", conf.Addr)
+		if err := r.Run(conf.Addr); err != nil {
+			logger.Error("start http server failed:%v", err)
+		}
+
+	}()
+
+	return nil
+}
+func setupRouter() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.Use(logger.LoggerDebug())
+	router.Use(gin.Recovery())
+
+	registerAPIs(router)
+
+	return router
+}
+func registerAPIs(router *gin.Engine) {
+	logger.Debug("router register")
+	plugin.GlobalClient.RegisterHandlers(router)
+
+	DBTarget := router.Group("/plugin/" + plugin.GlobalClient.PluginInfo.Name)
+	{
+		DBTarget.GET("", func(ctx *gin.Context) {
+			ctx.JSON(200, `{"hello","world"}`)
+		})
+	}
+}
