c71cabb47b5b808b98591f4f16bb44979d287a0b
Gzx1999 <guozhengxin@kylinos.cn>
2023-09-13 18:47:57 +0800

    add host select

--- topology/web/src/views/NodeView.vue
+++ topology/web/src/views/NodeView.vue
@@ -1,21 +1,38 @@
 <template>
-  <h1 class="h1">单机拓扑图演示页面</h1>
-  <div id="topo-container" class="container"></div>
+  <div class="title">
+    <h1 class="h1">单机拓扑图演示页面</h1>
+    <el-dropdown class="dropdown" @command="handleNodeSelected">
+      <span class="el-dropdown-link">
+        选择主机
+        <el-icon class="el-icon--right">
+          <arrow-down />
+        </el-icon>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item :command=node v-for="node in node_list">{{node.id}}</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
 
+  <div id="topo-container" class="container"></div>
   <el-drawer class="drawer" v-model="drawer" :title="title" :direction="direction" :before-close="handleClose">
-    <span>Hi, there!</span>
+    <span>Hi, there</span>
   </el-drawer>
 </template>
 
 <script setup lang="ts">
 import G6 from '@antv/g6';
-import { ref, onMounted } from "vue";
+import { ref,reactive, onMounted } from "vue";
 import { topo } from '../request/api';
 import server_logo from "@/assets/icon/server.png";
 
 let drawer = ref(false)
 const direction = 'rtl'
 const title = ref("")
+const node_list = reactive<any>([])
+let graph = ref()
 
 function handleClose() {
   drawer.value = false
@@ -23,8 +40,27 @@ function handleClose() {
 
 onMounted(async () => {
   try {
-    const data = await topo.single_host_tree("d6900229-971b-482c-b1a5-cf108777c6db");
-    console.log(data.data.tree);
+    updateNodeList()
+
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+async function updateNodeList() {
+  const data = await topo.host_list()
+  // console.log(data);
+  for (let key in data.data.agentlist){
+    node_list.push({
+      id: key,
+    })
+  };
+
+}
+
+async function handleNodeSelected(node: any) {
+  const data = await topo.single_host_tree(node.id);
+    // console.log(data.data.tree);
 
     let root = data.data.tree
     root.img = server_logo;
@@ -32,13 +68,10 @@ onMounted(async () => {
     root.size = 40
 
     initGraph(data.data.tree);
-  } catch (error) {
-    console.error(error)
-  }
-})
+}
 
 function initGraph(data: any) {
-  let graph = new G6.TreeGraph({
+  graph.value = new G6.TreeGraph({
     container: "topo-container",
     width: document.getElementById("topo-container")!.clientWidth,
     height: document.getElementById("topo-container")!.clientHeight,
@@ -61,7 +94,7 @@ function initGraph(data: any) {
       rankSep: 100,
     },
   });
-  graph.node(function (node: any) {
+  graph.value.node(function (node: any) {
     // console.log(node);
     return {
       label: node.node.type + ":" + node.node.name,
@@ -71,7 +104,7 @@ function initGraph(data: any) {
       },
     };
   });
-  graph.on("nodeselectchange", (e) => {
+  graph.value.on("nodeselectchange", (e) => {
     if (e.select) {
       let node = (e.target as any)._cfg!;
       let node_id = node.id
@@ -84,24 +117,43 @@ function initGraph(data: any) {
     }
     return false
   });
-  graph.data(data);
-  graph.render();
-  graph.fitCenter();
+  graph.value.data(data);
+  graph.value.render();
+  graph.value.fitCenter();
 
   window.onresize = () => {
-    graph.changeSize(
+    graph.value.changeSize(
       document.getElementById("topo-container")!.clientWidth,
       document.getElementById("topo-container")!.clientHeight)
-    graph.fitCenter()
+    graph.value.fitCenter()
   }
 }
 
 </script>
 
 <style scoped>
+
+.title {
+  position:relative;
+}
 .h1 {
   width: 100%;
+  margin: 0;
+  padding-top: 10px;
+  padding-bottom: 10px;
   text-align: center;
+}
+
+.dropdown {
+  font-size: 120%;
+  position: absolute;
+  background-color: white;
+  right: 0;
+  bottom: 0;
+
+  margin-bottom: 3px;
+  margin-right: 10px;
+  padding-left: 10px;
 }
 
 .container {
