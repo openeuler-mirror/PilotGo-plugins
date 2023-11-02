52d5b32258150b57d143ea08fb1062065e813765
zhanghan2021 <zhanghan@kylinos.cn>
2023-11-02 16:43:32 +0800

    Configure path aliases

--- atune/web/vite.config.ts
+++ atune/web/vite.config.ts
@@ -1,19 +1,46 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') //在任何模块文件内部，可以使用__dirname变量获取当前模块文件所在目录的完整绝对路径。
-    }
+      '@': path.resolve(__dirname, 'src'), //在任何模块文件内部，可以使用__dirname变量获取当前模块文件所在目录的完整绝对路径。
+    },
   },
-  server:{
-    host:'0.0.0.0',
-    port:8080
+  server: {
+    port: 8080,
+    https: false,
+    proxy: {
+      '/plugin': {
+        target: 'http://localhost:8099',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/plugin/, ''),
+      },
+    },
   },
-})
-
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static/', //指定静态资源存放路径
+    sourcemap: false, //是否构建source map 文件
+    chunkSizeWarningLimit: 1500, //chunk 大小警告的限制，默认500KB
+    rollupOptions: {
+      output: {
+        // 最小化拆分包
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+        chunkFileNames: 'js/[name].[hash].js', // 用于命名代码拆分时创建的共享块的输出命名，[name]表示文件名,[hash]表示该文件内容hash值
+      },
+    },
+  },
+});
