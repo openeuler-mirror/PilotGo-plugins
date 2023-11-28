a91c923f601a42f304c94e57d7b4eb30176c7515
zhanghan2021 <zhanghan@kylinos.cn>
2023-11-28 16:37:06 +0800

    Change the page size of the iTunes template page

--- atune/web/src/components/atuneTemplete.vue
+++ atune/web/src/components/atuneTemplete.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <p>{{ selectedNodeData }}</p>
+
+        <div>获取调优信息</div>
         <button @click="fetchAtuneInfo">获取调优信息</button>
 
         <el-form :model="form" label-width="120px">
@@ -27,13 +29,13 @@
 </template>
 
 <script lang='ts' setup>
-import { ref, onUpdated, reactive} from 'vue'
+import { ref, onUpdated, reactive } from 'vue'
 import { getAtuneInfo } from '@/api/atune'
 
-let props=defineProps({
-    selectedNodeData:{
-        type:String,
-        default:""
+let props = defineProps({
+    selectedNodeData: {
+        type: String,
+        default: ""
     }
 })
 
@@ -41,30 +43,29 @@ const atuneName = ref(props.selectedNodeData)
 // 使用 onUpdated 钩子来监视 selectedNodeData 的变化
 onUpdated(() => {
     atuneName.value = props.selectedNodeData
-    console.log(atuneName.value)
 })
 
 const atuneInfo = ref(null)
 const form = reactive({
-    tuneName:"",
-    workDir:"",
-    perpare:"",
-    tune:"",
-    restore:"",
-    note:""
+    tuneName: "",
+    workDir: "",
+    perpare: "",
+    tune: "",
+    restore: "",
+    note: ""
 })
-const fetchAtuneInfo=()=>{
+const fetchAtuneInfo = () => {
     if (atuneName.value) {
         // 获取单个模板信息
         getAtuneInfo({ name: atuneName.value })
-          .then((res) => {
-            if (res.data && res.data.code === 200){
-                atuneInfo.value = res.data; 
-                console.log('获取到的调优信息：', atuneInfo.value);
-            }else{
-                console.log('获取调优信息时出错:', res.data.msg)   
-            }
-          })
+            .then((res) => {
+                if (res.data && res.data.code === 200) {
+                    atuneInfo.value = res.data;
+                    console.log('获取到的调优信息：', atuneInfo.value);
+                } else {
+                    console.log('获取调优信息时出错:', res.data.msg)
+                }
+            })
     } else {
         console.warn('atuneName 为空，无法获取调优信息');
     }
@@ -72,7 +73,4 @@ const fetchAtuneInfo=()=>{
 
 </script>
 
-<style lang = 'less' scoped>
-
- 
-</style>
+<style lang = 'less' scoped></style>
