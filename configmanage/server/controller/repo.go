e265b1d59f4a3094a246394582aa2014094bdbb7
wubijie <wubijie@kylinos.cn>
2023-12-07 15:46:18 +0800

    add repoconfig fetch and update function

--- configmanage/server/controller/repo.go
+++ configmanage/server/controller/repo.go
@@ -10,7 +10,14 @@ import (
 
 func AddRepoHandler(c *gin.Context) {
 	//TODO:query 类型需要转变
-	var query int
+	query := struct {
+		Description string   `json:"description"`
+		BatchIds    []uint   `json:"batchids"`
+		DepartIds   []int    `json:"departids"`
+		Nodes       []string `json:"uuids"`
+		Name        string   `json:"name"`
+		File        string   `json:"file"`
+	}{}
 	err := c.ShouldBind(query)
 	if err != nil {
 		response.Fail(c, gin.H{"status": false}, err.Error())
@@ -20,8 +27,8 @@ func AddRepoHandler(c *gin.Context) {
 	configuuid := uuid.New().String()
 	config := &service.RepoConfig{
 		UUID: configuuid,
-		Name: "",
-		File: "",
+		Name: query.Name,
+		File: query.File,
 	}
 	err = config.Record()
 	if err != nil {
@@ -32,10 +39,10 @@ func AddRepoHandler(c *gin.Context) {
 	ci := service.ConfigInstance{
 		UUID:        uuid.New().String(),
 		Type:        global.Repo,
-		Description: "",
-		BatchIds:    []uint{},
-		DepartIds:   []int{},
-		Nodes:       []string{},
+		Description: query.Description,
+		BatchIds:    query.BatchIds,
+		DepartIds:   query.DepartIds,
+		Nodes:       query.Nodes,
 		Config:      config,
 	}
 	err = ci.Add(configuuid)
@@ -43,23 +50,43 @@ func AddRepoHandler(c *gin.Context) {
 		response.Fail(c, gin.H{"status": false}, err.Error())
 		return
 	}
+	response.Success(c, nil, "Add repo config success")
 }
 
 func GetRepoConfig(c *gin.Context) {
-	//TODO:query 类型需要转变需要包含uuid
-	var query int
+	//TODO:query 类型需要转变需要包含configuuid
+	query := c.Query("configuuid")
+	config := &service.RepoConfig{
+		UUID: query,
+	}
+	err := config.Load()
+	if err != nil {
+		response.Fail(c, gin.H{"status": false}, err.Error())
+		return
+	}
+	response.Success(c, config, "Get repo file success")
+}
+
+func UpdateRepoConfig(c *gin.Context) {
+	//TODO:query 类型需要转变需要包含configuuid
+	query := service.RepoConfig{}
 	err := c.ShouldBind(query)
 	if err != nil {
 		response.Fail(c, gin.H{"status": false}, err.Error())
 		return
 	}
-	config := &service.RepoConfig{
-		UUID: "configuuid",
-	}
-	err = config.Load()
+	configuuid := query.UUID
+	query.UUID = uuid.New().String()
+	err = query.Record()
 	if err != nil {
 		response.Fail(c, gin.H{"status": false}, err.Error())
 		return
 	}
 
+	err = query.UpdateRepoConfig(configuuid)
+	if err != nil {
+		response.Fail(c, gin.H{"status": false}, err.Error())
+		return
+	}
+	response.Success(c, query, "Update repo file success")
 }
