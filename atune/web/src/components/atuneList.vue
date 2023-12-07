9e0e4440a9df97f2cb406f9093b7a423ff331be8
zhanghan2021 <zhanghan@kylinos.cn>
2023-12-07 15:20:05 +0800

    Set pagination and table layout styles, add details of precautions

--- atune/web/src/components/atuneList.vue
+++ atune/web/src/components/atuneList.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="table">
-    <el-table :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" />
-      <el-table-column prop="tuneName" label="名称" width="150" />
-      <el-table-column prop="prepare" label="环境准备" width="370" />
-      <el-table-column prop="tune" label="调优" width="400" />
-      <el-table-column prop="restore" label="环境恢复" width="370" />
-      <el-table-column label="操作" width="80">
-        <template #default="{ row }">
-          <el-button type="primary" size="small" @click="handleEdit(row)">编辑</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
-  <div class="pagination">
-    <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[10, 20, 50]"
-      :total="totalItems" layout="total, sizes, prev, pager, next, jumper" @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"></el-pagination>
+  <div class="table-container">
+    <div class="table-wrapper">
+      <el-table :data="tableData" style="width: 100%;height:90%" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" />
+        <el-table-column type="expand" width="30">
+          <template #default="props">
+            <div class="expand-cell">
+              <p class="note-title">注意事项:</p>
+              <div class="note-content">{{ props.row.note }}</div>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="tuneName" label="名称" width="150" />
+        <el-table-column prop="prepare" label="环境准备" width="370" />
+        <el-table-column prop="tune" label="调优" width="400" />
+        <el-table-column prop="restore" label="环境恢复" width="370" />
+        <el-table-column label="操作" width="80">
+          <template #default="{ row }">
+            <el-button type="primary" size="small" @click="handleEdit(row)">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="pagination">
+      <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[10, 20, 50]"
+        :total="totalItems" layout="total, sizes, prev, pager, next, jumper" @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"></el-pagination>
+    </div>
   </div>
 </template>
 
@@ -113,21 +123,44 @@ watch(() => [props.refreshData, props.searchTune], ([refreshData, searchTune]): 
 </script>
 
 <style lang = 'less' scoped>
-.table {
+.table-container {
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
 
-  .el-table th,
-  .el-table td {
-    text-align: center; // 内容居中
-  }
-
-  .el-table th .cell {
-    font-weight: bold; // 设置标签为黑体
-  }
+.table-wrapper {
+  overflow: auto;
+  height: 82vh; 
+  margin-left: 10px;
 }
 
 .pagination {
   position: absolute;
   bottom: 0;
-  padding-bottom: 3%;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  z-index: 1;
+}
+
+.expand-cell {
+  display: flex;
+  flex-direction: column;
+  width: 96%;
+  margin-left: auto;
+}
+
+.note-title {
+  word-wrap: break-word; /* 换行 */
+}
+
+.note-content {
+  word-wrap: break-word; 
+  line-height: 1.8;
+  padding-left: 2em;
+  white-space: pre-line;
 }
 </style>
