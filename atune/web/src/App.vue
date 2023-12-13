5c4ceadba92be4ee2839f995bb43d792fbe3f017
zhanghan2021 <zhanghan@kylinos.cn>
2023-12-13 14:18:16 +0800

    Write a list of returned result information

--- atune/web/src/App.vue
+++ atune/web/src/App.vue
@@ -17,21 +17,29 @@ const zhCn = ref(locale);
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  height: 98vh;
-  overflow: hidden; /* Ensure no scrollbars on the app container */
+  height: 100%;
+  width: 100%;
+  // overflow: hidden; /* Ensure no scrollbars on the app container */
 }
 
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  background-color: rgb(11, 35, 117);
-  overflow: hidden; /* Ensure no scrollbars on the body */
-  width: 100vw;
-  transform-origin: 0 0 0;
-  -moz-transform-origin: 0 0 0;
-  -ms-transform-origin: 0 0 0;
-  -webkit-transform-origin: 0 0 0;
-  -o-transform-origin: 0 0 0;
-}
+// html {
+//   margin: 0;
+//   padding: 0;
+//   height: 100%;
+//   width: 100%;
+// }
+
+// body {
+//   margin: 0;
+//   padding: 0;
+//   height: 100%;
+//   width: 100%;
+//   background-color: rgb(11, 35, 117);
+//   // overflow: hidden; /* Ensure no scrollbars on the body */
+//   transform-origin: 0 0 0;
+//   -moz-transform-origin: 0 0 0;
+//   -ms-transform-origin: 0 0 0;
+//   -webkit-transform-origin: 0 0 0;
+//   -o-transform-origin: 0 0 0;
+// }
 </style>
