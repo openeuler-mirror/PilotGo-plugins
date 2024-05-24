11eb2235b0e6957e875e19acd95a6cdfbc651303
Wangjunqi123 <wangjunqi@kylinos.cn>
2024-05-24 10:27:39 +0800

    add handler module to the elk plugin

--- elk/main.go
+++ elk/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"gitee.com/openeuler/PilotGo-plugin-elk/conf"
-	"gitee.com/openeuler/PilotGo-plugin-elk/pluginclient"
 	"gitee.com/openeuler/PilotGo-plugin-elk/errormanager"
+	"gitee.com/openeuler/PilotGo-plugin-elk/pluginclient"
+	"gitee.com/openeuler/PilotGo-plugin-elk/handler"
 )
 
 func main() {
@@ -21,4 +22,9 @@ func main() {
 		init error control
 	*/
 	errormanager.InitErrorManager()
+
+	/*
+		init web server
+	*/
+	handler.InitWebServer()
 }
