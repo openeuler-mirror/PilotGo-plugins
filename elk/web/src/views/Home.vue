bc5cc1517583289223f446ecd0f828da37eac083
zhaozhenfang <zhaozhenfang@kylinos.cn>
2024-06-11 14:29:44 +0800

    elk web init

---
+++ elk/web/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    home页面
+  </div>
+</template>
+
+<script setup lang="ts">
+import { testHttps } from '../api';
+
+const test = () => {
+  testHttps().then(res => {
+    console.log(res)
+  })
+}
+test();
+</script>
+
+<style scoped></style>
---
+++ elk/web/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import {resolve} from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: '/plugin/modeule_name/', // 生产环境下的公共路径
+  server: {
+    host:'10.41.107.33',
+    port: 8080,
+    strictPort: false, // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+    cors: true,
+    open:true, // 启动后是否浏览器自动打开
+    hmr:true, // 为开发服务启用热更新，默认是不启用热更新的
+    /* proxy: {
+      '/': {
+        target: 'https://10.41.107.29:9993',
+        secure: false,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\//, '')
+      },
+    }, */
+  },
+  plugins: [vue(),],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@assetImg':resolve(__dirname,'src/assets/images') 
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static/', //指定静态资源存放路径
+    sourcemap: false, //是否构建source map 文件
+    chunkSizeWarningLimit: 1500, //chunk 大小警告的限制，默认500KB
+  },
+});
