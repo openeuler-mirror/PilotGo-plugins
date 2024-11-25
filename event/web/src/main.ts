a215b0dcee4521cb146d5b0182699b8322b55662
赵振芳 <zhaozhenfang@kylinos.cn>
2024-11-25 17:18:25 +0800

    chore：install element-plus

--- event/web/src/main.ts
+++ event/web/src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+export const app = createApp(App)
+app.use(ElementPlus)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.mount('#app')
