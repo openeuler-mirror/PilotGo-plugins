77a42e7133b03db6e322ec0184a0e71166362beb
zhanghan <zhanghan@kylinos.cn>
2025-09-04 17:32:30 +0800

    init mysql service connect

---
+++ automation/server/internal/service/app/mysql.go
@@ -0,0 +1,84 @@
+package app
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"openeuler.org/PilotGo/PilotGo-plugin-automation/cmd/config/options"
+	"openeuler.org/PilotGo/PilotGo-plugin-automation/internal/global"
+)
+
+type MySQLService struct {
+	Conf *options.MysqlDBInfo
+}
+
+func (m *MySQLService) Name() string {
+	return "MySQL"
+}
+
+func (m *MySQLService) Init(ctx *global.AppContext) error {
+	err := ensureDatabase(m.Conf)
+	if err != nil {
+		return err
+	}
+
+	dsn := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=true",
+		m.Conf.UserName,
+		m.Conf.Password,
+		m.Conf.HostName,
+		m.Conf.Port,
+		m.Conf.DataBase)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	ctx.MySQL = db
+	return nil
+}
+
+func (m *MySQLService) Close() error {
+	if global.App.MySQL == nil {
+		return nil
+	}
+	sqlDB, err := global.App.MySQL.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Close()
+}
+
+func ensureDatabase(conf *options.MysqlDBInfo) error {
+	dsn := fmt.Sprintf("%s:%s@(%s:%d)/?charset=utf8mb4&parseTime=true",
+		conf.UserName,
+		conf.Password,
+		conf.HostName,
+		conf.Port)
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		return err
+	}
+
+	createDBSQL := fmt.Sprintf(
+		"CREATE DATABASE IF NOT EXISTS `%s` DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci",
+		conf.DataBase,
+	)
+	if err := db.Exec(createDBSQL).Error; err != nil {
+		return fmt.Errorf("create database error: %w", err)
+	}
+
+	d, err := db.DB()
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	return nil
+}
