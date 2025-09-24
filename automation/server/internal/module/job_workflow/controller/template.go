f421aa7a952a7bcb403bd019c4ceab4bb4146848
zhanghan <zhanghan@kylinos.cn>
2025-09-24 13:34:46 +0800

    Create script orchestration template handler

---
+++ automation/server/internal/module/job_workflow/controller/template.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"openeuler.org/PilotGo/PilotGo-plugin-automation/internal/module/job_workflow/model"
+	"openeuler.org/PilotGo/PilotGo-plugin-automation/internal/module/job_workflow/service"
+	"openeuler.org/PilotGo/PilotGo-plugin-automation/pkg/response"
+)
+
+func CreateTemplate(c *gin.Context) {
+	var template model.TaskTemplateDTO
+	if err := c.ShouldBindJSON(&template); err != nil {
+		response.Fail(c, nil, err.Error())
+		return
+	}
+	if err := service.CreateTemplate(&template); err != nil {
+		response.Fail(c, nil, err.Error())
+		return
+	}
+	response.Success(c, nil, "success")
+}
---
+++ automation/server/internal/module/job_workflow/router.go
@@ -0,0 +1,13 @@
+package jobworkflow
+
+import (
+	"github.com/gin-gonic/gin"
+	"openeuler.org/PilotGo/PilotGo-plugin-automation/internal/module/job_workflow/controller"
+)
+
+func WorkflowHandler(router *gin.RouterGroup) {
+	api := router.Group("/workflows")
+	{
+		api.POST("/create", controller.CreateTemplate)
+	}
+}
